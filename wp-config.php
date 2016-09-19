3e70682a23d279063775d1cfea6484399fe7bb65
Sergey Dmitrievskiy <dmitrievskiy-serega@yandex.ru>
2016-09-19 20:13:03 +0300

    правки в конфиге

--- wp-config.php
+++ wp-config.php
@@ -21,17 +21,19 @@ define('WP_DEBUG', false);
 @ini_set('display_errors', 1);
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
-/** Имя базы данных для WordPress */
+
+//development
 define('DB_NAME', 'arena-space_local');
-
-/** Имя пользователя MySQL */
 define('DB_USER', 'root');
-
-/** Пароль к базе данных MySQL */
 define('DB_PASSWORD', 'root');
-
-/** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
+
+//production
+//define('DB_NAME', 'arspaceru_db');
+//define('DB_USER', 'arspaceru_mysql');
+//define('DB_PASSWORD', 'En4zGpQ+');
+//define('DB_HOST', 'arspaceru.mysql');
+
 
 /** Кодировка базы данных для создания таблиц. */
 define('DB_CHARSET', 'utf8');
